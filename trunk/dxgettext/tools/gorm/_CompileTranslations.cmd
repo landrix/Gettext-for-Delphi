4070ef8d629d7e0ed6451926496a771c342e1412
twm <twm@39591dab-e61a-0410-a3c9-9e61e257b166>
2014-02-15 18:59:20 +0000

    buildtools + batch files for updating/compiling German translations for Gorm

---
+++ trunk/dxgettext/tools/gorm/_CompileTranslations.cmd
@@ -0,0 +1,3 @@
+..\..\..\buildtools\msgfmt translations\de\default.po -o ..\..\output\debug\gorm\locale\de\lc_messages\default.mo
+..\..\..\buildtools\msgfmt translations\de\default.po -o ..\..\output\release\gorm\locale\de\lc_messages\default.mo
+pause
---
+++ trunk/dxgettext/tools/gorm/_UpdateTranslations.cmd
@@ -0,0 +1,5 @@
+del default.po
+..\..\..\buildtools\dxgettext.exe --delphi
+..\..\..\buildtools\msgmerge translations\de\default.po default.po -o de.po
+move de.po translations\de\default.po
+pause
