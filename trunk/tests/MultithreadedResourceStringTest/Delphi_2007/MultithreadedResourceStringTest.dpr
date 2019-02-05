ad2491579a22f27d766e0746437576c8bc337cf5
twm <twm@39591dab-e61a-0410-a3c9-9e61e257b166>
2019-02-05 14:56:43 +0000

    Copied Delphi 2007 project

---
+++ trunk/tests/MultithreadedResourceStringTest/Delphi_2007/MultithreadedResourceStringTest.dpr
@@ -0,0 +1,17 @@
+program MultithreadedResourceStringTest;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  gnugettext in '..\..\..\dxgettext\sample\gnugettext.pas',
+  MultithreadedResourceStringTestMain in '..\src\MultithreadedResourceStringTestMain.pas';
+
+begin
+  try
+    Main;
+  except
+    on E:Exception do
+      Writeln(E.Classname, ': ', E.Message);
+  end;
+end.
