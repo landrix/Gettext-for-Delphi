77fd6816ac0fe63585385787403b7b0376ddfaf6
twm <twm@39591dab-e61a-0410-a3c9-9e61e257b166>
2019-03-10 10:39:09 +0000

    Test projects that only compile sample\gnugettext.pas to easily find compile errors, warnings and hints for all supported versions of Delphi.

---
+++ trunk/dxgettext/CompileTest/Delphi2007/src/GnuGettextCompileTest.dpr
@@ -0,0 +1,11 @@
+program GnuGettextCompileTest;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  gnugettext in '..\..\..\sample\gnugettext.pas';
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+end.
